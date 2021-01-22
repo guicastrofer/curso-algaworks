0cf775249e75dfd1e4248ea57dd42ccb9513af74
guicastrofer <42876310+guicastrofer@users.noreply.github.com>
2021-01-22 08:15:05 -0300

    My first commit
    My first commit

---
+++ src/test/java/com/br/curso/algaworks/AlgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.curso.algaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
