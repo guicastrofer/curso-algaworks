0eca30173c9e8a53320bb7ee01f2b79de4c98cb5
guicastrofer <42876310+guicastrofer@users.noreply.github.com>
2021-02-10 08:48:50 -0300

    Criação de método http Post e Get

--- src/main/java/com/br/curso/algaworks/api/controller/ClienteController.java
+++ src/main/java/com/br/curso/algaworks/api/controller/ClienteController.java
@@ -3,26 +3,42 @@ package com.br.curso.algaworks.api.controller;
 import com.br.curso.algaworks.domain.model.Cliente;
 import com.br.curso.algaworks.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
+
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 
     @Autowired
     ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar() {
         return clienteRepository.findAll();
     }
 
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 
-
-
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente inserirCliente(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
 }
--- src/main/java/com/br/curso/algaworks/repository/ClienteRepository.java
+++ src/main/java/com/br/curso/algaworks/repository/ClienteRepository.java
@@ -4,7 +4,9 @@ import com.br.curso.algaworks.domain.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
-
+        public List<Cliente> findByNome(String nome);
 }
